21845eda1c561e2d3e7fab12e0b1cc60e83cfd16
Oleksandr <sash1983direct@gmail.com>
2025-09-07 16:28:10 -0700

    dz12

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
   root: 'src',          
   base: '/goit-js-hw-12/', 
   build: {
-    outDir: 'docs',  
+    outDir: '../dist',  
   },
   define: {
     global: 'window',
